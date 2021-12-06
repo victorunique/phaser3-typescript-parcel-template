dcd8352c9c8b645170c846b3518fef3766988aa6
Victor Xu <victor.unique@gmail.com>
2021-12-06 10:45:02 +0000

    Create an example.

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,12 @@ const config: Phaser.Types.Core.GameConfig = {
 	width: 800,
 	height: 600,
 	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	},
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: true
+        }
+    },
 	scene: [HelloWorldScene]
 }
 
--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -1,39 +1,145 @@
-import Phaser from 'phaser'
+import Phaser, { Physics } from 'phaser'
 
-export default class HelloWorldScene extends Phaser.Scene
-{
-	constructor()
-	{
+export default class HelloWorldScene extends Phaser.Scene {
+    private platforms?: Phaser.Physics.Arcade.StaticGroup
+    private player?: Phaser.Physics.Arcade.Sprite
+    private cursors?: Phaser.Types.Input.Keyboard.CursorKeys
+    private stars?: Phaser.Physics.Arcade.Group
+    private bombs?: Phaser.Physics.Arcade.Group
+
+    private score = 0;
+    private scoreText?: Phaser.GameObjects.Text;
+    private gameOver = false;
+
+	constructor() {
 		super('hello-world')
 	}
 
-	preload()
-    {
-        this.load.setBaseURL('http://labs.phaser.io')
-
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
+	preload() {
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     }
 
-    create()
-    {
-        this.add.image(400, 300, 'sky')
+    create() {
+        // background
+        this.add.image(400, 300, 'sky');
 
-        const particles = this.add.particles('red')
+        // build the world
+        this.platforms = this.physics.add.staticGroup();
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
 
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
+        // create the sprite
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
 
-        const logo = this.physics.add.image(400, 100, 'logo')
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
 
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'dude', frame: 4 } ],
+            frameRate: 20
+        });
 
-        emitter.startFollow(logo)
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        }); 
+
+        this.physics.add.collider(this.player, this.platforms);
+
+        // configure the keyboard control
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        // stars to collect
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: { x: 12, y: 0, stepX: 70 }
+        });
+        
+        this.stars.children.iterate((c) => {
+            const child = c as Phaser.Physics.Arcade.Image
+            child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+        });
+
+        this.physics.add.collider(this.stars, this.platforms);
+        this.physics.add.overlap(this.player, this.stars, this.collectStar, undefined, this);
+
+        // scoring
+        this.scoreText = this.add.text(16, 16, 'Score: 0', { 
+            fontSize: '32px', 
+            color: '#F1F' 
+        });
+
+        // bomb
+        this.bombs = this.physics.add.group();
+
+        this.physics.add.collider(this.bombs, this.platforms);
+        this.physics.add.collider(this.player, this.bombs, this.hitBomb, undefined, this);
+    }
+
+    update() {
+        if (!this.cursors || !this.player) {
+            return;
+        }
+
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+            this.player.anims.play('left', true);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+            this.player.anims.play('right', true);
+        } else {
+            this.player.setVelocityX(0);
+            this.player.anims.play('turn');
+        }
+        
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
+    }
+
+    private collectStar(p: Phaser.GameObjects.GameObject, s: Phaser.GameObjects.GameObject) {
+        const star = s as Phaser.Physics.Arcade.Image
+        star.disableBody(true, true);
+
+        this.score += 10;
+        this.scoreText?.setText('Score: ' + this.score);
+
+        if (this.stars?.countActive(true) === 0) {
+            this.stars.children.iterate((c) => {
+                const child = c as Phaser.Physics.Arcade.Image
+                child.enableBody(true, child.x, 0, true, true);
+            });
+
+            const player = p as Phaser.Physics.Arcade.Sprite
+            var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+            var bomb = this.bombs?.create(x, 16, 'bomb');
+            bomb.setBounce(1);
+            bomb.setCollideWorldBounds(true);
+            bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+        }
+    }
+
+    private hitBomb(player, bomb) {
+        this.physics.pause();
+        player.setTint(0xff0000);
+        player.anims.play('turn');
+        this.gameOver = true;
     }
 }
